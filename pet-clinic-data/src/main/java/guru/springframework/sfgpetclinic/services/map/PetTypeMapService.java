b31a69608921f7320aecd2f5e8d7d9395f8eb1e3
mibihi <mibiixi@gmail.com>
2020-03-17 18:34:08 -0500

    specialtiesmap service added

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -15,7 +15,7 @@ public class PetTypeMapService extends  AbstractMapService<PetType, Long>  imple
 
     @Override
     public void deleteById(Long id) {
-
+  super.deleteById(id);
     }
 
     @Override
